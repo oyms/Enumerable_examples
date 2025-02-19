38fae56f2b5e6f186efd441c409b1337f2f2157b
Øyvind Skaar <oyvind.skaar@bekk.no>
2025-02-19 20:20:18 +0100

    Filesystemwatcher example

---
+++ FileSystemwatcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using FileSystemwatcher;
+
+using var cts = new CancellationTokenSource();
+Console.WriteLine("Press any key to stop");
+_ = Task.Run(() =>
+{
+    Console.ReadKey(true);
+    cts.Cancel();
+});
+
+
+using var watcher = new Watcher();
+try
+{
+    await foreach (var fileEvent in watcher.WithCancellation(cts.Token))
+    {
+        Console.WriteLine(fileEvent.Text);
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Bye! 👋");
+}
---
+++ FileSystemwatcher/Watcher.cs
@@ -0,0 +1,46 @@
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+
+namespace FileSystemwatcher;
+
+public class Watcher: IAsyncEnumerable<FileSystemEvent>, IDisposable
+{
+    private readonly Channel<FileSystemEvent> _channel = Channel.CreateUnbounded<FileSystemEvent>();
+    private FileSystemWatcher? _watcher;
+    
+    public IAsyncEnumerator<FileSystemEvent> GetAsyncEnumerator(CancellationToken cancellationToken = new()) => 
+        GetEventsAsync(cancellationToken).GetAsyncEnumerator(cancellationToken);
+
+    private void Init()
+    {
+        _watcher = new FileSystemWatcher(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))
+            {
+                IncludeSubdirectories = true,
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName,
+                Filter = "*.*"
+            };
+        _watcher.Created += (_, e) => _channel.Writer.TryWrite(new FileSystemEvent($"File created: {e.FullPath}"));
+        _watcher.Deleted += (_, e) => _channel.Writer.TryWrite(new FileSystemEvent($"File deleted: {e.FullPath}"));
+        _watcher.Changed += (_, e) => _channel.Writer.TryWrite(new FileSystemEvent($"File changed: {e.FullPath} {e.ChangeType}"));
+        _watcher.Renamed += (_, e) => _channel.Writer.TryWrite(new FileSystemEvent($"File renamed: {e.FullPath} from {e.OldFullPath}"));
+        _watcher.EnableRaisingEvents = true;
+    }
+
+    private async IAsyncEnumerable<FileSystemEvent> GetEventsAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        if (_watcher is null) Init();
+        
+        while (await _channel.Reader.WaitToReadAsync(cancellationToken))
+        {
+            yield return await _channel.Reader.ReadAsync(cancellationToken);
+        }
+    }
+
+    public void Dispose()
+    {
+        _watcher?.Dispose();
+        _channel.Writer.TryComplete();
+    }
+}
+
+public record FileSystemEvent(string Text);
